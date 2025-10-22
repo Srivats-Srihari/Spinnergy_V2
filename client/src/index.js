3d5f6e515b56b8b931eb39c7b7de4c7c9344882c
Srivats-Srihari <ss.astrofacts@gmail.com>
2025-10-22 13:27:49 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import "./styles/App.css";
+import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
